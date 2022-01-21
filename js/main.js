ae19056032c6c17b18f4ed2921e96e2da37b5f71
unknown <shelite17@gmail.com>
2022-01-21 02:22:38 -0700

    Updated about page

--- js/main.js
+++ js/main.js
@@ -51,4 +51,15 @@ $(document).ready(function(){
         adaptiveHeight: true,
         arrows: true,
     });
+    $('.slideshow-art').slick({
+        infinite: true,
+        arrows: false,
+        speed: 9000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+    });
 });
